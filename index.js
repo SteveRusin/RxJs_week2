6270c96f4213a45aec44cee466f9e48dd9b102a5
Steve <steve.neeson21@gmail.com>
2018-02-15 22:04:15 +0200

    task4 done

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 // https://jsonplaceholder.typicode.com/ 
 
-const { fromPromise, interval, mergeDelayError, merge, timeout, empty, timeoutWithSelector } = Rx.Observable;
+const { of } = Rx.Observable;
 
 
-const reject = interval(5000).map(x => new Error('Timeout!'));
-const resolve = function (id) {
-    id++;
-    if (id > 10) return;
-    return fromPromise(fetch(`https://jsonplaceholder.typicode.com/users/${id}`))
-        .timeout(200)
-        .flatMap(res => res.json())
-        .subscribe(
-            (resolve) => {
-                console.log(resolve)
-            },
-            null,
-            () => {
-                resolve(id)
-            }
-        );
+class Subject extends Rx.Observable {
+    constructor() {
+        super();
+        this.observes = [];
+    }
+
+    subscribe(observer) {
+        this.observes.push(observer)
+    }
+
+    next(value) {
+        this.observes.forEach(observer => observer.next(value));
+    }
+
+    error(error) {
+        this.observes.forEach(observer => observer.error(error));
+    }
+
+    complete() {
+        this.observes.forEach(observer => observer.complete());
+    }
 }
-resolve(0)
+
+const subject = new Subject();
+
+subject.map(x => x).subscribe(x => console.log(x))
+
+subject.next('25')
+subject.next('15')
+subject.next(of([15,16]))
